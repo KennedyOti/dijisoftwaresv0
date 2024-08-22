07bb0ce373c0dbc2b2f76287c19d1237966df462
Dijisoftwares <dijisoftwares@gmail.com>
2024-08-22 13:50:48 +0300

    contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,122 +1,129 @@
 @extends('layouts.app')
 
 @section('content')
-
-  <!-- Contact Start -->
-  <div class="container-fluid contact py-5">
-    <div class="container py-5">
-        <div class="row g-5">
-            <div class="col-xl-6">
-                <div class="wow fadeInUp" data-wow-delay="0.2s">
-                    <div class="bg-light rounded p-5 mb-5">
-                        <h4 class="text-primary mb-4">Get in Touch</h4>
-                        <div class="row g-4">
-                            <div class="col-md-6">
-                                <div class="contact-add-item">
-                                    <div class="contact-icon text-primary mb-4">
-                                        <i class="fas fa-map-marker-alt fa-2x"></i>
-                                    </div>
-                                    <div>
-                                        <h4>Address</h4>
-                                        <p class="mb-0">123 Street New York.USA</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="contact-add-item">
-                                    <div class="contact-icon text-primary mb-4">
-                                        <i class="fas fa-envelope fa-2x"></i>
-                                    </div>
-                                    <div>
-                                        <h4>Mail Us</h4>
-                                        <p class="mb-0">info@example.com</p>
+    <!-- Contact Start -->
+    <div class="container-fluid contact py-5">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-xl-6">
+                    <div class="wow fadeInUp" data-wow-delay="0.2s">
+                        <div class="bg-light rounded p-5 mb-5">
+                            <h4 class="text-primary mb-4">Get in Touch</h4>
+                            <div class="row g-4">
+                                <div class="col-md-6">
+                                    <div class="contact-add-item">
+                                        <div class="contact-icon text-primary mb-4">
+                                            <i class="fas fa-map-marker-alt fa-2x"></i>
+                                        </div>
+                                        <div>
+                                            <h4>Address</h4>
+                                            <p class="mb-0">Njoro, Egerton University</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="contact-add-item">
-                                    <div class="contact-icon text-primary mb-4">
-                                        <i class="fa fa-phone-alt fa-2x"></i>
-                                    </div>
-                                    <div>
-                                        <h4>Telephone</h4>
-                                        <p class="mb-0">(+012) 3456 7890</p>
+                                <div class="col-md-6">
+                                    <div class="contact-add-item">
+                                        <div class="contact-icon text-primary mb-4">
+                                            <i class="fas fa-envelope fa-2x"></i>
+                                        </div>
+                                        <div>
+                                            <h4>Mail Us</h4>
+                                            <p class="mb-0">info@dijisoftwares.com</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="contact-add-item">
-                                    <div class="contact-icon text-primary mb-4">
-                                        <i class="fab fa-firefox-browser fa-2x"></i>
+                                <div class="col-md-6">
+                                    <div class="contact-add-item">
+                                        <div class="contact-icon text-primary mb-4">
+                                            <i class="fa fa-phone-alt fa-2x"></i>
+                                        </div>
+                                        <div>
+                                            <h4>Telephone</h4>
+                                            <p class="mb-0">+254793543659</p>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <h4>Yoursite@ex.com</h4>
-                                        <p class="mb-0">(+012) 3456 7890</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="contact-add-item">
+                                        <div class="contact-icon text-primary mb-4">
+                                            <i class="fab fa-firefox-browser fa-2x"></i>
+                                        </div>
+                                        <div>
+                                            <h4>dijisoftwares.com</h4>
+                                            <p class="mb-0">+254793543659</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="bg-light p-5 rounded h-100 wow fadeInUp" data-wow-delay="0.2s">
-                        <h4 class="text-primary">Send Your Message</h4>
-                        <p class="mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a class="text-primary fw-bold" href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
-                        <form>
-                            <div class="row g-4">
-                                <div class="col-lg-12 col-xl-6">
-                                    <div class="form-floating">
-                                        <input type="text" class="form-control border-0" id="name" placeholder="Your Name">
-                                        <label for="name">Your Name</label>
+                        <div class="bg-light p-5 rounded h-100 wow fadeInUp" data-wow-delay="0.2s">
+                            <h4 class="text-primary">Send Your Message</h4>
+                            <p class="mb-4">The contact form is currently inactive. Get a functional and working contact
+                                form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and
+                                you're done. <a class="text-primary fw-bold"
+                                    href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
+                            <form>
+                                <div class="row g-4">
+                                    <div class="col-lg-12 col-xl-6">
+                                        <div class="form-floating">
+                                            <input type="text" class="form-control border-0" id="name"
+                                                placeholder="Your Name">
+                                            <label for="name">Your Name</label>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-lg-12 col-xl-6">
-                                    <div class="form-floating">
-                                        <input type="email" class="form-control border-0" id="email" placeholder="Your Email">
-                                        <label for="email">Your Email</label>
+                                    <div class="col-lg-12 col-xl-6">
+                                        <div class="form-floating">
+                                            <input type="email" class="form-control border-0" id="email"
+                                                placeholder="Your Email">
+                                            <label for="email">Your Email</label>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-lg-12 col-xl-6">
-                                    <div class="form-floating">
-                                        <input type="phone" class="form-control border-0" id="phone" placeholder="Phone">
-                                        <label for="phone">Your Phone</label>
+                                    <div class="col-lg-12 col-xl-6">
+                                        <div class="form-floating">
+                                            <input type="phone" class="form-control border-0" id="phone"
+                                                placeholder="Phone">
+                                            <label for="phone">Your Phone</label>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-lg-12 col-xl-6">
-                                    <div class="form-floating">
-                                        <input type="text" class="form-control border-0" id="project" placeholder="Project">
-                                        <label for="project">Your Project</label>
+                                    <div class="col-lg-12 col-xl-6">
+                                        <div class="form-floating">
+                                            <input type="text" class="form-control border-0" id="project"
+                                                placeholder="Project">
+                                            <label for="project">Your Project</label>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="form-floating">
-                                        <input type="text" class="form-control border-0" id="subject" placeholder="Subject">
-                                        <label for="subject">Subject</label>
+                                    <div class="col-12">
+                                        <div class="form-floating">
+                                            <input type="text" class="form-control border-0" id="subject"
+                                                placeholder="Subject">
+                                            <label for="subject">Subject</label>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="form-floating">
-                                        <textarea class="form-control border-0" placeholder="Leave a message here" id="message" style="height: 160px"></textarea>
-                                        <label for="message">Message</label>
-                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-floating">
+                                            <textarea class="form-control border-0" placeholder="Leave a message here" id="message" style="height: 160px"></textarea>
+                                            <label for="message">Message</label>
+                                        </div>
 
+                                    </div>
+                                    <div class="col-12">
+                                        <button class="btn btn-primary w-100 py-3">Send Message</button>
+                                    </div>
                                 </div>
-                                <div class="col-12">
-                                    <button class="btn btn-primary w-100 py-3">Send Message</button>
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-xl-6 wow fadeInRight" data-wow-delay="0.2s">
-                <div class="rounded h-100">
-                    <iframe class="rounded h-100 w-100" 
-                    style="height: 400px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.33750346623!2d-73.97968099999999!3d40.6974881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1694259649153!5m2!1sen!2sbd" 
-                    loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="col-xl-6 wow fadeInRight" data-wow-delay="0.2s">
+                    <div class="rounded h-100">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.734479591031!2d35.93846307568523!3d-0.3714406353119773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182987e76dbabbe9%3A0x302acc4e835baac3!2sEgerton%20University%20(Njoro%20)-Main%20Campus!5e0!3m2!1sen!2ske!4v1724323736950!5m2!1sen!2ske"
+                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<!-- Contact End -->
-
+    <!-- Contact End -->
 @endsection
