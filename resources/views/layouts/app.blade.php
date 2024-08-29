1fff95d17c7836af95312bca4b877d8fe320c13f
Dijisoftwares <dijisoftwares@gmail.com>
2024-08-29 17:46:46 +0300

    update on the coursel section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -142,7 +142,8 @@
                     <!-- Carousel Start -->
                     <div class="header-carousel owl-carousel">
                         <div class="header-carousel-item">
-                            <img src="{{ asset('assets/img/coursel1.jpg') }}" class="img-fluid w-100" alt="Image">
+                            <img src="{{ asset('assets/img/coursel1.jpg') }}" class="img-fluid w-100"
+                                alt="Image">
                             <div class="carousel-caption">
                                 <div class="container">
                                     <div class="row gy-0 gx-5">
@@ -192,6 +193,61 @@
                                 </div>
                             </div>
                         </div>
+
+                        <div class="header-carousel-item">
+                            <img src="{{ asset('assets/img/coursel14.jpg') }}" class="img-fluid w-100"
+                                alt="Image">
+                            <div class="carousel-caption">
+                                <div class="container">
+                                    <div class="row gy-0 gx-5">
+                                        <div class="col-lg-0 col-xl-5"></div>
+                                        <div class="col-xl-7 animated fadeInLeft">
+                                            <div class="text-sm-center text-md-end">
+                                                <h4 class="main-color text-uppercase fw-bold mb-4">Welcome To
+                                                    Dijisoftwares
+                                                </h4>
+                                                <h1 class="display-4 text-uppercase text-white mb-4">Digital Literacy
+                                                    and Career Training Program
+                                                </h1>
+                                                <p class="mb-5 fs-5">The Digital Literacy and Career Training Program
+                                                    is designed to empower secondary school students with essential
+                                                    digital skills and career guidance. Our goal is to bridge the
+                                                    digital divide and help students make informed decisions about their
+                                                    academic and career paths.
+                                                </p>
+                                                <div
+                                                    class="d-flex justify-content-center justify-content-md-end flex-shrink-0 mb-4">
+                                                    <a class="btn btn-light rounded-pill py-3 px-4 px-md-5 me-2"
+                                                        href="#"><i class="fas fa-play-circle me-2"></i> Watch
+                                                        Video</a>
+                                                    <a class="btn main-bg rounded-pill py-3 px-4 px-md-5 ms-2"
+                                                        href="{{ route('apply') }}">Go to Program Website</a>
+                                                </div>
+                                                <div
+                                                    class="social-icons d-flex align-items-center justify-content-center justify-content-md-end">
+                                                    <h2 class="text-white me-2">Follow Us:</h2>
+                                                    <div class="d-flex justify-content-end ms-2">
+                                                        <a class="btn btn-md-square btn-light rounded-circle me-2"
+                                                            href="">
+                                                            <i class="fab fa-facebook-f"></i></a>
+                                                        <a class="btn btn-md-square btn-light rounded-circle mx-2"
+                                                            href="">
+                                                            <i class="fab fa-twitter"></i></a>
+                                                        <a class="btn btn-md-square btn-light rounded-circle mx-2"
+                                                            href="">
+                                                            <i class="fab fa-instagram"></i></a>
+                                                        <a class="btn btn-md-square btn-light rounded-circle ms-2"
+                                                            href="">
+                                                            <i class="fab fa-linkedin-in"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                         <div class="header-carousel-item">
                             <img src="{{ asset('assets/img/coursel2.jpg') }}" class="img-fluid w-100"
                                 alt="Image">
@@ -203,8 +259,7 @@
                                                 <h4 class="main-color text-uppercase fw-bold mb-4">Welcome To
                                                     Dijisoftwares
                                                 </h4>
-                                                <h1 class="display-4 text-uppercase text-white mb-4">Full Stack Web
-                                                    Apps</h1>
+                                                <h1 class="display-4 text-uppercase text-white mb-4">We Develop Software Solutions For Businesses</h1>
                                                 <p class="mb-5 fs-5">At dijisoftwares we use modern web technologies to
                                                     develop fast and reliable full
                                                     stack web application that run seamlessly and help you achieve your
